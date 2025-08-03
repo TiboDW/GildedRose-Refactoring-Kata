303c0c14bbcc2a832d92faaffee5988cebc59577
Tibo De Winter <tibo_dewinter@hotmail.com>
2025-08-03 16:02:57 +0200

    Implement updateItemConjured

--- TypeScript/app/update.ts
+++ TypeScript/app/update.ts
@@ -38,7 +38,23 @@ export const updateItemSulfuras = (item: Item) => {
   item.quality = 80;
 };
 
-export const updateItemConjured = (item: Item) => {};
+export const updateItemConjured = (item: Item) => {
+  if (item.quality > MIN_QUALITY) {
+    item.quality -= 2;
+    if (item.quality < MIN_QUALITY) {
+      item.quality = MIN_QUALITY;
+    }
+  }
+
+  item.sellIn--;
+
+  if (item.sellIn < MIN_QUALITY && item.quality > MIN_QUALITY) {
+    item.quality -= 2;
+    if (item.quality < MIN_QUALITY) {
+      item.quality = MIN_QUALITY;
+    }
+  }
+};
 
 export const updateItem = (item: Item) => {
   if (item.quality > MIN_QUALITY) {
