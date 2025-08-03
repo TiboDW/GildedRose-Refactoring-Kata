8d73b5efa79a4600c9a7274bc3a674c681251e9b
Tibo De Winter <tibo_dewinter@hotmail.com>
2025-08-03 17:00:18 +0200

    Update increase and decrease qualitiy functions, remove unnecessary checks

--- TypeScript/app/update.ts
+++ TypeScript/app/update.ts
@@ -9,34 +9,30 @@ const decreaseSellIn = (item: Item, amount: number = 1) => {
   item.sellIn -= amount;
 };
 const increaseQuality = (item: Item, amount: number = 1) => {
-  item.quality += amount;
+  item.quality = Math.min(MAX_QUALITY, item.quality + amount);
 };
 const decreaseQuality = (item: Item, amount: number = 1) => {
-  item.quality -= amount;
+  item.quality = Math.max(MIN_QUALITY, item.quality - amount);
 };
 
 export const updateItemBrie = (item: Item) => {
-  if (item.quality < MAX_QUALITY) {
-    increaseQuality(item);
-  }
+  increaseQuality(item);
 
   decreaseSellIn(item);
 
-  if (item.sellIn < MIN_QUALITY && item.quality < MAX_QUALITY) {
+  if (item.sellIn < MIN_QUALITY) {
     increaseQuality(item);
   }
 };
 
 export const updateItemPass = (item: Item) => {
-  if (item.quality < MAX_QUALITY) {
-    increaseQuality(item);
+  increaseQuality(item);
 
-    if (item.sellIn < 11 && item.quality < MAX_QUALITY) {
-      increaseQuality(item);
-    }
-    if (item.sellIn < 6 && item.quality < MAX_QUALITY) {
-      increaseQuality(item);
-    }
+  if (item.sellIn < 11) {
+    increaseQuality(item);
+  }
+  if (item.sellIn < 6) {
+    increaseQuality(item);
   }
 
   decreaseSellIn(item);
@@ -51,31 +47,21 @@ export const updateItemSulfuras = (item: Item) => {
 };
 
 export const updateItemConjured = (item: Item) => {
-  if (item.quality > MIN_QUALITY) {
-    decreaseQuality(item, 2);
-    if (item.quality < MIN_QUALITY) {
-      item.quality = MIN_QUALITY;
-    }
-  }
+  decreaseQuality(item, 2);
 
   decreaseSellIn(item);
 
-  if (item.sellIn < MIN_QUALITY && item.quality > MIN_QUALITY) {
+  if (item.sellIn < MIN_QUALITY) {
     decreaseQuality(item, 2);
-    if (item.quality < MIN_QUALITY) {
-      item.quality = MIN_QUALITY;
-    }
   }
 };
 
 export const updateItem = (item: Item) => {
-  if (item.quality > MIN_QUALITY) {
-    decreaseQuality(item);
-  }
+  decreaseQuality(item);
 
   decreaseSellIn(item);
 
-  if (item.sellIn < MIN_QUALITY && item.quality > MIN_QUALITY) {
+  if (item.sellIn < MIN_QUALITY) {
     decreaseQuality(item);
   }
 };
--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -8,3 +8,12 @@ describe("Gilded Rose", () => {
     expect(items[0].name).toBe("foo");
   });
 });
+
+describe("test one", () => {
+  it("Update quality selling for one day", () => {
+    const gildedRose = new GildedRose([new Item("foo", 1, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+    expect(items[0].sellIn).toBe(0);
+  });
+});
