3b279abbc04716a0bb739f265bb82f527537d9c8
Tibo De Winter <tibo_dewinter@hotmail.com>
2025-08-03 15:40:47 +0200

    Update item logic in updateItem function and the updateSulfuras function is kept empty because their is no logic

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -49,8 +49,20 @@ const updateItemPass = (item: Item) => {
 };
 
 const updateItemSulfuras = (item: Item) => {};
+
 const updateItemConjured = (item: Item) => {};
-const updateItem = (item: Item) => {};
+
+const updateItem = (item: Item) => {
+  if (item.quality > 0) {
+    item.quality = item.quality - 1;
+  }
+
+  item.sellIn = item.sellIn - 1;
+
+  if (item.sellIn < 0 && item.quality > 0) {
+    item.quality = item.quality - 1;
+  }
+};
 
 export class GildedRose {
   items: Array<Item>;
