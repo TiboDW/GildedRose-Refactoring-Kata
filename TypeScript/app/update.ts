b766c1989632bd1b08b869261699d28af4222870
Tibo De Winter <tibo_dewinter@hotmail.com>
2025-08-03 15:49:01 +0200

    Remove magic numbers add MAX_QUALITY and MIN_QUALITY

--- TypeScript/app/update.ts
+++ TypeScript/app/update.ts
@@ -1,32 +1,35 @@
 import { Item } from "./gilded-rose";
 
+const MAX_QUALITY = 50;
+const MIN_QUALITY = 0;
+
 export const updateItemBrie = (item: Item) => {
-  if (item.quality < 50) {
+  if (item.quality < MAX_QUALITY) {
     item.quality = item.quality + 1;
   }
 
   item.sellIn = item.sellIn - 1;
 
-  if (item.sellIn < 0 && item.quality < 50) {
+  if (item.sellIn < MIN_QUALITY && item.quality < MAX_QUALITY) {
     item.quality = item.quality + 1;
   }
 };
 
 export const updateItemPass = (item: Item) => {
-  if (item.quality < 50) {
+  if (item.quality < MAX_QUALITY) {
     item.quality = item.quality + 1;
 
-    if (item.sellIn < 11 && item.quality < 50) {
+    if (item.sellIn < 11 && item.quality < MAX_QUALITY) {
       item.quality = item.quality + 1;
     }
-    if (item.sellIn < 6 && item.quality < 50) {
+    if (item.sellIn < 6 && item.quality < MAX_QUALITY) {
       item.quality = item.quality + 1;
     }
   }
 
   item.sellIn = item.sellIn - 1;
 
-  if (item.sellIn < 0) {
+  if (item.sellIn < MIN_QUALITY) {
     item.quality = item.quality - item.quality;
   }
 };
@@ -36,13 +39,13 @@ export const updateItemSulfuras = (item: Item) => {};
 export const updateItemConjured = (item: Item) => {};
 
 export const updateItem = (item: Item) => {
-  if (item.quality > 0) {
+  if (item.quality > MIN_QUALITY) {
     item.quality = item.quality - 1;
   }
 
   item.sellIn = item.sellIn - 1;
 
-  if (item.sellIn < 0 && item.quality > 0) {
+  if (item.sellIn < MIN_QUALITY && item.quality > MIN_QUALITY) {
     item.quality = item.quality - 1;
   }
 };
