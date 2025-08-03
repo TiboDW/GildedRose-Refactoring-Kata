818a3908ef15a2290d0e7f2cee9bcb7597d8ef66
Tibo De Winter <tibo_dewinter@hotmail.com>
2025-08-03 15:32:23 +0200

    Update pass logic in updateItemPass function

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -28,7 +28,26 @@ const updateItemBrie = (item: Item) => {
     item.quality = item.quality + 1;
   }
 };
-const updateItemPass = (item: Item) => {};
+
+const updateItemPass = (item: Item) => {
+  if (item.quality < 50) {
+    item.quality = item.quality + 1;
+
+    if (item.sellIn < 11 && item.quality < 50) {
+      item.quality = item.quality + 1;
+    }
+    if (item.sellIn < 6 && item.quality < 50) {
+      item.quality = item.quality + 1;
+    }
+  }
+
+  item.sellIn = item.sellIn - 1;
+
+  if (item.sellIn < 0) {
+    item.quality = item.quality - item.quality;
+  }
+};
+
 const updateItemSulfuras = (item: Item) => {};
 const updateItemConjured = (item: Item) => {};
 const updateItem = (item: Item) => {};
